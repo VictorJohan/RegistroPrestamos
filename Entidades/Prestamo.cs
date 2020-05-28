8c208e37cbe1e25c79a1e704d969634668d7c7cb
Victor Johan <Vicjohan23@gmail.com>
2020-05-28 02:09:09 -0400

    SE CREO LA CARPETA "ENTIDADES"

---
+++ Entidades/Prestamo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RegistroPrestamos.Entidades
+{
+    public class Prestamo
+    {
+        [Key]
+        public int IdPersona { get; set; }
+        public int IdPrestamo { get; set; }
+        public double Monto { get; set; }
+        public double Balance { get; set; }
+        public string ConceptoPrestamo { get; set; }
+        public DateTime FechaPrestamo { get; set; } = DateTime.Now;
+
+    }
+}
